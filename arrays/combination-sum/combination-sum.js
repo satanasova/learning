b4e8f75a489b3f6d1e51c4df383c8f6a8eb2f24a
Siyana Atanasova <s.e.atanasowa@gmail.com>
2020-01-27 01:25:04 +0200

    description fixed

--- arrays/combination-sum/combination-sum.js
+++ arrays/combination-sum/combination-sum.js
@@ -1,9 +1,6 @@
 // Description
-// Write a program that finds in given array of integers a sequence of given sum S (if present).
-
-// Sample tests
-// array	              S	     result
-// 4, 3, 1, 4, 2, 5, 8	  11	 4, 2, 5
+// Write a program that finds in given array of integers all combinations
+// for given sum S (if present).
 
 
 function binaryCombo(arr, S) {
