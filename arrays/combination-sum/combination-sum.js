0299eeae2f14deff524e3eb45f5d55055af2a062
Siyana Atanasova <s.e.atanasowa@gmail.com>
2020-01-27 01:06:39 +0200

    filter method added

--- arrays/combination-sum/combination-sum.js
+++ arrays/combination-sum/combination-sum.js
@@ -5,37 +5,11 @@
 // array	              S	     result
 // 4, 3, 1, 4, 2, 5, 8	  11	 4, 2, 5
 
-// function binaryCombos(n){
-//     var result = [];
-//     for(y=0; y<Math.pow(2,n); y++){
-//         var combo = [];
-//         for(x=0; x<=n; x++){
-//             //shift bit and and it with 1
-//             if((y >> x) & 1)
-//                 combo.push(1);
-//              else 
-//                 combo.push(0);
-//         }
-//         result.push(combo);
-//     }
-//     return result;
-// }
-
-// //Usage
-// combos = binaryCombos(3);
-
-// for(x=0; x<combos.length; x++){
-//     console.log(combos[x].join(','));
-// }
-
-// binaryCombos(4);
-// console.log(Math.pow(2,4));
 
 function binaryCombo(arr, S) {
     const binaryLength = Math.pow(2, arr.length),
         combos = [];
     // console.log(binaryLength);
-    let sum = 0;
     for (let i = 0; i < binaryLength; i++) {
         let singleCombo = [];
         let binaryStr = i.toString(2).padStart(arr.length, '0');
@@ -50,13 +24,14 @@ function binaryCombo(arr, S) {
         combos.push(singleCombo);
     }
 
-    console.log(combos);
-    for (let combo of combos){
-        sum = combo.reduce((a, b) => (a + b), 0);
-        if(sum === S){
-            console.log(combo);
-        }
-    }
+    const combosSums = combos.map(arr => ({
+        arr,
+        sum: arr.reduce((sum, cur) => (sum + cur), 0)
+        })
+    )
+    combosSums.filter(arr => arr.sum === S).forEach(arr => console.log(arr.arr));
 }
 
-binaryCombo([13, 6, 16, 28, 34, 66, 3,2,7], 11);
+
+
+binaryCombo([4, 3, 1, 4, 2, 5, 8], 11);
