21432fcae3c5c28d1e58b89b74f3410133b67544
Atanas Atanasov <hackohackob@gmail.com>
2020-01-27 16:32:24 +0200

    - fixed description
    - refactoring

--- arrays/combination-sum/combination-sum.js
+++ arrays/combination-sum/combination-sum.js
@@ -1,6 +1,5 @@
 // Description
-// Write a program that finds in given array of integers all combinations
-// for given sum S (if present).
+// Write a program that finds in given array of integers all combinations of sums that are equal to a given number (if present)
 
 
 function binaryCombo(arr, S) {
@@ -12,23 +11,20 @@ function binaryCombo(arr, S) {
         let binaryStr = i.toString(2).padStart(arr.length, '0');
         // console.log(binaryStr);
         for (let j = 0; j < binaryStr.length; j++) {
-            if(binaryStr[j] === '1'){
+            if (binaryStr[j] === '1') {
                 // console.log(args[j]);
                 singleCombo.push(arr[j]);
-            } 
-            
+            }
         }
+
         combos.push(singleCombo);
     }
 
     const combosSums = combos.map(arr => ({
         arr,
         sum: arr.reduce((sum, cur) => (sum + cur), 0)
-        })
-    )
+    }));
     combosSums.filter(arr => arr.sum === S).forEach(arr => console.log(arr.arr));
 }
-
-
 
 binaryCombo([4, 3, 1, 4, 2, 5, 8], 11);
