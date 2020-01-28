3efe6c6c957aaf8acd595b3b540739ec2c07b2e0
Siyana Atanasova <s.e.atanasowa@gmail.com>
2020-01-28 03:19:51 +0200

    added frequent number

---
+++ arrays/telerik/frequent-number/README.md
@@ -0,0 +1,12 @@
+Find the most frequent number in an array of N elements.
+
+---
+Output:
+- Print the most frequent number and how many time it is repeated
+    - Output should be REPEATING_NUMBER (REPEATED_TIMES times)
+
+---
+Sample test:
+
+    Input: ['13', '4', '1', '1', '4', '2', '3', '4', '4', '1', '2', '4', '9', '3']
+    Output: 4 (5 times)
---
+++ arrays/telerik/frequent-number/frequent-number.js
@@ -0,0 +1,21 @@
+function findMostFreq(args) {
+    const [len,...arr] = args.map(Number);
+    let repNumber = arr[0],
+        repTimes = 0,
+        maxRepTimes = 1;
+    for (let i = 0; i < len; i++) {
+        for (let j = i; j < len; j++) {
+            if (arr[i] == arr[j]) {
+                repTimes++;
+            }
+            if (maxRepTimes < repTimes) {
+                maxRepTimes = repTimes;
+                repNumber = arr[i];
+            }
+        }
+        repTimes = 0; 
+    }
+    return `${repNumber} (${maxRepTimes} times)`;
+    }
+
+console.log(findMostFreq(['13', '4', '1', '1', '4', '2', '3', '4', '4', '1', '2', '4', '9', '3']));
