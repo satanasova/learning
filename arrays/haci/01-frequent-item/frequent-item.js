bcb32d8746f03225242493e2e3f14ce6ec8d81fd
Siyana Atanasova <s.e.atanasowa@gmail.com>
2020-01-28 04:52:42 +0200

    added frequent item

---
+++ arrays/haci/01-frequent-item/frequent-item.js
@@ -0,0 +1,23 @@
+function findMostFreq(arr) {
+    let repItem,
+        repTimes = 0,
+        maxRepTimes = 1;
+        
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                repTimes++;
+            }
+            if (maxRepTimes < repTimes) {
+                maxRepTimes = repTimes;
+                repItem = arr[i];
+            }
+        }
+        repTimes = 0; 
+    }
+
+    return repItem ? repItem : 'None';
+}
+
+console.log(findMostFreq(['pufi', 'malko pi', 'abchi', 'mili', 'mili', 'mili', 'mili', 'mili', '4', '4', '4', '3', '4', '3']));
+console.log(findMostFreq(['mili', 'pufi', 'malko pi', 'abchi', 'tigyr', 'jerry', 'bichi', 'richi']));
