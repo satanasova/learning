718bd93fd5c1c5fda537c0fdd0d5dc0ac7efe340
Siyana Atanasova <s.e.atanasowa@gmail.com>
2020-01-28 09:07:52 +0200

    added max equal sequence

--- arrays/telerik/max-equal-sequence/max-equal-sequence.js
+++ arrays/telerik/max-equal-sequence/max-equal-sequence.js
@@ -1,19 +1,18 @@
 function maxEqualSeq(args) {
-    const [len, ...arr] = args;
+    const [len, ...arr] = args.map(Number);
     let maxLen = 1,
         currLen = 1;
 
     for (let i = 0; i < len-1; i++) {
-            // console.log(arr[i+1] + '-' + arr[i]);
             if (arr[i+1] === arr[i]) {
                 currLen++
             } else currLen = 1;
-            // console.log(currLength);
             if (currLen > maxLen) {
                 maxLen = currLen;
             }
     } 
-    return maxLength>1? maxLength : 0;
+    
+    return maxLen > 1 ? maxLen : 0;
 }
 
 console.log(maxEqualSeq(['10', '2', '1', '1', '2', '3', '3', '2', '2', '2', '1']));
