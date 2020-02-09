3cce9c6fedddb9b509eae47ebd505d0e0a44e657
Siyana Atanasova <s.e.atanasowa@gmail.com>
2020-02-09 20:09:14 +0200

    added permutation

--- arrays/telerik/permutation/permutation.js
+++ arrays/telerik/permutation/permutation.js
@@ -1,1 +1,35 @@
 // НЕ МОГА
+
+function solve(N) {
+    const permArr = [],
+        usedChars = [],
+        arr = [];
+        
+    for (let i = 1; i <= N; i++) {
+        arr.push(i);
+    }
+
+    function permute(input) {
+        // преписан и неразбран алгоритъм 
+        let ch;
+        for (let i = 0; i < input.length; i++) {
+            ch = input.splice(i, 1)[0];
+            usedChars.push(ch);
+
+            if (input.length == 0) {
+                permArr.push(usedChars.slice());
+            }
+
+            permute(input);
+            input.splice(i, 0, ch);
+            usedChars.pop();
+        }
+
+        return permArr
+    }
+
+    return permute(arr)
+}
+
+
+console.log(solve(3));
